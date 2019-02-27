94f685dfd3b49606471728a6e83defa0d7003fe6
contactservis <skiminok2005@gmail.com>
2019-02-27 09:13:02 +0300

    Решение в MVC, с классами

--- index.php
+++ index.php
@@ -1,26 +1,38 @@
 <?php
-function getItemRSS($Count)
-{
-    // url  до источника rss
-    $lenta_url_rss = 'https://lenta.ru/rss';
-    // количество выводимых записей
-    $count_view_item = $Count;
+// модель
+class GetItemLentaRSS{
+    public function getItem($url, $count){
+        $lenta_content = file_get_contents($url);
+        $lenta_item_rss = new SimpleXmlElement($lenta_content);
 
-    $lenta_content = file_get_contents($lenta_url_rss);
-    $lenta_item_rss = new SimpleXmlElement($lenta_content);
+        $arr_content_rss = array();
 
-    $arr_content_rss = array();
+        foreach ($lenta_item_rss->channel->item as $item) {
+            $arr_content_rss[] = $item;
+        }
+        // обрезаем массив до нужного нам количества записей
+        $arr_content_rss = array_slice($arr_content_rss, 0, $count);
 
-    foreach ($lenta_item_rss->channel->item as $item) {
-        $arr_content_rss[] = $item;
+        return $arr_content_rss;
     }
-
-    // обрезаем массив до нужного нам количчества записей
-    $arr_content_rss = array_slice($arr_content_rss, 0, $count_view_item);
-    return $arr_content_rss;
 }
 
-$arr_content_rss = getItemRSS(3);
+// контроллер
+
+class ViewItemRSS
+{
+    public function viewItem($count)
+    {
+        $ItemRSS = new GetItemLentaRSS();
+        $arr_content_rss = $ItemRSS->getItem('https://lenta.ru/rss', $count);
+        return $arr_content_rss;
+    }
+}
+
+// представление
+$ViewItem = new ViewItemRSS();
+$arr_content_rss = $ViewItem->viewItem(5);
+
 
 foreach ($arr_content_rss as $item){
     ?>
